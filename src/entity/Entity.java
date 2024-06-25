6f4de8174995b091499d260ccf6a4ff332b64e5d
Robbi Firmansyah <muhamadrobbifirmansyah@mail.ugm.ac.id>
2024-06-25 17:41:08 +0700

    collision

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -2,12 +2,11 @@ package entity;
 
 import java.awt.image.BufferedImage;
 
-public class Entity {
+abstract class Entity {
     public int worldX, worldY;
     public int speed;
 
     public BufferedImage up, up1, up2, up3, down, down1, down2, down3, left, left1, left2, left3, right, right1, right2, right3;
     public String direction;
-
 
 }
